3dc1f0baf691566f538c909bf3bac3756832ff07
VitorBWang <166049961+VitorBWang@users.noreply.github.com>
2024-10-21 20:00:05 -0300

    Delete src directory

--- src/Frontend/home.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('alertButton').addEventListener('click', function() {
-      alert('Bem-vindo ao VitalForm! O melhor site para você melhorar a sua saúde!');
-    });
-  });
-  
-
