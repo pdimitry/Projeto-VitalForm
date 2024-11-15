e6bb868905dd1a81a385b2424f707e55109accd3
pdimitry <166882296+pdimitry@users.noreply.github.com>
2024-11-15 15:36:01 -0300

    Add files via upload

---
+++ vitalform/src/components/header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoNavBar from '../assets/imagens/imagem-logo/logo.png';
+import './header.css'; 
+
+const Header = () => (
+    <header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <div className="navbar-brand d-flex align-items-center"> {/* Usar d-flex para alinhar logo e texto */}
+                    <img src={logoNavBar} alt="Logo VitalForm" height="40" width="40" className="me-2" /> {/* Classe me-2 para margem à direita */}
+                    <Link className="nav-link" to="/">VitalForm</Link>
+                </div>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/nutrição">Nutrição</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/comunidade">Comunidade</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/fitness">Fitness</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/bemestar">Bem Estar</Link>
+                        </li>
+                    </ul>
+                    
+                    <div className="navbar-buttons">
+                        <Link className="nav-link btn-cadastro" to="/cadastro">Cadastro</Link>
+                        <Link className="nav-link btn-login" to="/login">Login</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Header;
---
+++ vitalform/src/pages/fitness.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './fitness.css';
+import Treino from '../components/Treino';
+
+
+const Fitness = () => (
+  <div className="fitness">
+    <body>
+      <Treino/>
+    </body>
+  </div>
+);
+
+export default Fitness;
